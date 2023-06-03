e17c478e9b7b2da625d4a53e6c75e4809ffb4564
Roman Kolobych <romankol1223@gmail.com>
2023-06-03 19:58:15 +0300

    img updt

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -4,13 +4,17 @@ import { Link } from 'react-router-dom';
 const GalleryItem = ({ rating, state, path, poster, title }) => {
   const imageBaseUrl = 'https://image.tmdb.org/t/p/w400';
 
+  let posterSrc = '';
+  if (poster) {
+    posterSrc = `${imageBaseUrl}${poster}`;
+  }
+
   return (
     <li className="">
       <Link to={path} state={state}>
         <article>
           <div>
-            <img src={`${imageBaseUrl}${poster}`} alt={title} className="" />
-
+            {posterSrc && <img src={posterSrc} alt="" />}
             <p className="">{title}</p>
             <div className="">
               <p>Rating: {rating}</p>
